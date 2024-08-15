99c5e0334807bb63c44672225ecd54694363953e
danyswr <daniswara.173@gmail.com>
2024-08-15 07:38:16 +0700

    gw cuma belajar framework FLASK

--- framework.py
+++ framework.py
@@ -3,12 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Aku Suka Makan Nasi"
+def index():
+    return "aku sayang moonton, moonton sayang kami"
 
-@app.route('/about')
-def about():
-    return 'Ini adalah halaman About'
+@app.route('/profile/<nama>')
+def namakamu(nama):
+    return "halo kamu %s" %nama
 
 if __name__ == '__main__':
     app.run(debug=True)
